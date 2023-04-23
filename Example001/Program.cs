6d3bafef9d84d825a753a9b280568d0dd89056f9
rusakova slavyana <blondi_sl@mail.ru>
2023-04-23 13:24:48 +0300

    двумерный-массив

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Двумерный массив
+
+// string [,] table = new string[2, 5];
+// table[1,2] = "слово";
+
+// for (int rows = 0; rows < 2; rows++)
+// {
+//     for (int columns = 0; columns < 5; columns++)
+//     {
+//         Console.Write($"-{table[rows, columns]}- ");
+//     }
+// Console.WriteLine();
+// }
+
+
+// int [,] matrix = new int [3, 4];
+
+// for (int i = 0; i < 3; i++)
+// {
+//     for (int j = 0; j < 4; j++)
+//     {
+//         Console.Write($"{matrix[i, j]}  ");
+//     }
+// Console.WriteLine();
+// }
+
+
+// обернули в метод
+
+void PrintArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+        Console.Write($"{matr[i, j]}  ");
+        }
+Console.WriteLine();
+    }
+}
+
+void FillArray(int[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i, j] = new Random().Next(1,9);
+        }
+    }
+    Console.WriteLine();
+}
+
+int [,] matrix = new int [3, 4];
+PrintArray(matrix);
+FillArray(matrix);
+PrintArray(matrix);
+
+
