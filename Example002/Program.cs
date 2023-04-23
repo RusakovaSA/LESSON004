edeab601d5150038ebaa94f1c001bc23684ba3a1
rusakova slavyana <blondi_sl@mail.ru>
2023-04-23 13:52:12 +0300

    Рекупсия-Фибоначчи

--- Example002/Program.cs
+++ Example002/Program.cs
@@ -1,8 +1,23 @@
 ﻿// Рекурсия
+// Факториал
 
-int Factorial(int n)
+// int Factorial(int n)
+// {
+//     if (n == 1) return 1;
+//     else return n * Factorial(n - 1);
+// }
+// Console.WriteLine($"Факториал числа равен {Factorial(3)}.");
+
+// Фибоначи
+
+int Fibonacci(int n)
 {
-    if (n == 1) return 1;
-    else return n * Factorial(n - 1);
+    if (n==1 || n==2) return 1;
+    else return Fibonacci(n-1) + Fibonacci(n-2);
 }
-Console.WriteLine($"Факториал числа равен {Factorial(3)}.");
+
+Console.Write("Последовательность Фибоначчи ");
+for (int i = 1; i < 10; i++)
+{
+        Console.Write($"{Fibonacci(i)}, ");
+}
